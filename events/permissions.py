d1cd91bc49e57403fbe69ffb1e0ea49a2b963a66
Max Netyaga <max.netyaga@gmail.com>
2026-01-05 18:48:25 +0200

    Add ability to accept invites

--- events/permissions.py
+++ events/permissions.py
@@ -21,6 +21,28 @@ class OnlyEventOrganizerEditoOrDelete(BasePermission):
         return cast(User, obj.organizer).pk == request.user.pk
 
 
+class IsEventOrganizer(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if not isinstance(obj, Event):
+            raise Exception(
+                f"{self.__class__.__name__} can be used only with Event views."
+            )
+
+        return request.user.pk == cast(User, obj.organizer).pk
+
+
+class IsEventInvited(BasePermission):
+    message = "You are not invited to event"
+
+    def has_object_permission(self, request, view, obj):
+        if not isinstance(obj, Event):
+            raise Exception(
+                f"{self.__class__.__name__} can be used only with Event views."
+            )
+
+        return request.user.pk in (user.pk for user in obj.invites.all())
+
+
 class IsEventOrganizerOrInvited(BasePermission):
     def has_object_permission(self, request, view, obj):
         if not isinstance(obj, Event):
--- events/views.py
+++ events/views.py
@@ -1,8 +1,11 @@
-from django.db.models import Q
-from rest_framework import permissions, viewsets
+from django.shortcuts import get_object_or_404
+from drf_spectacular.utils import extend_schema
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from . import permissions as event_permissions
-from .models import Event
+from .models import Event, EventInvite
 from .serializers import EventSerializer
 
 
@@ -15,9 +18,32 @@ class EventViewSet(viewsets.ModelViewSet):
         event_permissions.OnlyEventOrganizerEditoOrDelete,
     ]
 
-    def get_queryset(self):
-        user = self.request.user
-        return Event.objects.filter(Q(organizer=user) | Q(invites=user)).distinct()
-
     def perform_create(self, serializer):
         serializer.save(organizer=self.request.user)
+
+    @extend_schema(request=None)
+    @action(
+        detail=True,
+        methods=["post"],
+        url_path="submit-invitation",
+        permission_classes=[event_permissions.IsEventInvited],
+    )
+    def submit_event_invitation(self, request, pk=None):
+        event = self.get_object()
+
+        invitation = get_object_or_404(
+            EventInvite,
+            user=request.user,
+            event=event,
+        )
+
+        if invitation.state == "ACCEPTED":
+            return Response(
+                {"detail": "Invitation already accepted."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        invitation.state = "ACCEPTED"
+        invitation.save(update_fields=["state"])
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
